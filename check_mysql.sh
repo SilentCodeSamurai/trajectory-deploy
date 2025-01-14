21c1ef0f027ad6d78ab03f1662e82f4af2b8aab6
SilentCodeSamurai <silentcodesamurai@gmail.com>
2025-01-14 09:08:57 +0300

    fix: script

--- check_mysql.sh
+++ check_mysql.sh
@@ -1,19 +1,43 @@
 #!/bin/bash
 
-# check_mysql.sh
+# mysql_cli.sh
 
-# Run the mysql_cli.sh script
-/root/trajectory-deploy/mysql_cli.sh
+# Load environment variables from .env file
+set -o allexport
+source .env
+set +o allexport
 
-# Check the exit status of the last command
-if [ $? -eq 1 ]; then
-    # Define email parameters
+SERVICE_NAME="${STACK_NAME}_db"
+
+# Get the task ID for the service
+TASK_ID=$(docker service ps --filter "desired-state=running" --format "{{.ID}}" "$SERVICE_NAME" | head -n 1)
+
+# Check if a task was found
+if [ -z "$TASK_ID" ]; then
+    echo "Error: No running task found for service '$SERVICE_NAME'."
+    exit 1
+fi
+
+# Get the container ID from the task ID
+CONTAINER_ID=$(docker inspect --format '{{.Status.ContainerStatus.ContainerID}}' "$TASK_ID")
+
+# Check if a container ID was found
+if [ -z "$CONTAINER_ID" ]; then
+    echo "Error: No container found for task ID '$TASK_ID'."
+    exit 1
+fi
+
+# Attempt to connect to the MySQL container
+if docker exec -it "$CONTAINER_ID" mysql -u root --password="$MYSQL_ROOT_PASSWORD" -e "exit"; then
+    echo "Login to MySQL successful."
+else
     TO="glazunovgennadyanatolyevitch@yandex.ru"  # Change to the recipient's email address
     SUBJECT="ALERT ALARM TREVOGA"
     BODY="Mysql DB FAILED"
-
+    
     # Send the email
     echo "$BODY" | mail -s "$SUBJECT" "$TO"
+    exit 1
 fi
 
 # */10 * * * * /root/trajectory-deploy/check_mysql.sh
